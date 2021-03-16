4aea8222508828f494fa9f6d999b02f5d4e87fad
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2021-03-16 10:34:45 -0400

    Initial commit

---
+++ Assets/BadgerScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadgerScript : MonoBehaviour {
+    public Texture[] cardTextures;
+    public Texture goalCard;
+    public GameObject cardTemplate;
+    public Transform spawnPoint, midPoint, endPoint;
+
+    public KMBombModule Module;
+    public KMSelectable LeftButton, RightButton, BadgerButton;
+
+    private const float VOFFSET = 0.0005f;
+    private Stack<GameObject> CardsRight = new Stack<GameObject>(), CardsLeft = new Stack<GameObject>();
+
+    private bool _solved = false;
+
+	// Use this for initialization
+	void Start () {
+        cardTextures.Shuffle();
+        for (int i = 0; i < cardTextures.Length; i++)
+        {
+            CardsRight.Push(Instantiate(cardTemplate, spawnPoint.position, spawnPoint.rotation, transform));
+            CardsRight.Peek().transform.localPosition = new Vector3(CardsRight.Peek().transform.localPosition.x, CardsRight.Peek().transform.localPosition.y + i * VOFFSET, CardsRight.Peek().transform.localPosition.z);
+            CardsRight.Peek().GetComponent<MeshRenderer>().material.mainTexture = cardTextures[i];
+            CardsRight.Peek().transform.localEulerAngles = new Vector3(90f, 0f, 180f);
+        }
+
+        LeftButton.OnInteract += Left;
+        RightButton.OnInteract += Right;
+        BadgerButton.OnInteract += Badger;
+	}
+
+    private bool Left()
+    {
+        if (CardsRight.Count == 0) return false;
+        StartCoroutine(MoveLeft(CardsRight.Peek(), CardsRight.Count * VOFFSET - VOFFSET, CardsLeft.Count * VOFFSET));
+        CardsLeft.Push(CardsRight.Pop());
+        return false;
+    }
+
+    private bool Right()
+    {
+        if (CardsLeft.Count == 0) return false;
+        StartCoroutine(MoveRight(CardsLeft.Peek(), CardsLeft.Count * VOFFSET - VOFFSET, CardsRight.Count * VOFFSET));
+        CardsRight.Push(CardsLeft.Pop());
+        return false;
+    }
+
+    private bool Badger()
+    {
+        if (_solved) return false;
+        if (CardsLeft.Count != 0 && CardsLeft.Peek().GetComponent<MeshRenderer>().material.mainTexture == goalCard)
+        {
+            Module.HandlePass();
+            _solved = true;
+        }
+        else
+        {
+            Module.HandleStrike();
+        }
+        return false;
+    }
+
+    private IEnumerator MoveLeft(GameObject card, float start, float end)
+    {
+        float time = 0f;
+        while (time < 0.25f)
+        {
+            yield return null;
+            time += Time.deltaTime;
+            card.transform.localPosition = new Vector3(Mathf.Lerp(spawnPoint.localPosition.x, midPoint.localPosition.x, time * 4f), Mathf.Lerp(spawnPoint.localPosition.y + start, midPoint.localPosition.y, time * 4f), Mathf.Lerp(spawnPoint.localPosition.z, midPoint.localPosition.z, time * 4f));
+            card.transform.localRotation = Quaternion.Lerp(spawnPoint.localRotation, midPoint.localRotation, time * 4f);
+        }
+        time = 0f;
+        while (time < 0.25f)
+        {
+            yield return null;
+            time += Time.deltaTime;
+            card.transform.localPosition = new Vector3(Mathf.Lerp(midPoint.localPosition.x, endPoint.localPosition.x, time * 4f), Mathf.Lerp(midPoint.localPosition.y, endPoint.localPosition.y + end, time * 4f), Mathf.Lerp(midPoint.localPosition.z, endPoint.localPosition.z, time * 4f));
+            card.transform.localRotation = Quaternion.Lerp(midPoint.localRotation, endPoint.localRotation, time * 4f);
+        }
+    }
+
+    private IEnumerator MoveRight(GameObject card, float start, float end)
+    {
+        float time = 0f;
+        while (time < 0.25f)
+        {
+            yield return null;
+            time += Time.deltaTime;
+            card.transform.localPosition = new Vector3(Mathf.Lerp(endPoint.localPosition.x, midPoint.localPosition.x, time * 4f), Mathf.Lerp(endPoint.localPosition.y + start, midPoint.localPosition.y, time * 4f), Mathf.Lerp(endPoint.localPosition.z, midPoint.localPosition.z, time * 4f));
+            card.transform.localRotation = Quaternion.Lerp(endPoint.localRotation, midPoint.localRotation, time * 4f);
+        }
+        time = 0f;
+        while (time < 0.25f)
+        {
+            yield return null;
+            time += Time.deltaTime;
+            card.transform.localPosition = new Vector3(Mathf.Lerp(midPoint.localPosition.x, spawnPoint.localPosition.x, time * 4f), Mathf.Lerp(midPoint.localPosition.y, spawnPoint.localPosition.y + end, time * 4f), Mathf.Lerp(midPoint.localPosition.z, spawnPoint.localPosition.z, time * 4f));
+            card.transform.localRotation = Quaternion.Lerp(midPoint.localRotation, spawnPoint.localRotation, time * 4f);
+        }
+    }
+}
---
+++ Assets/CardLayout.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardLayout : MonoBehaviour {
+    public Texture[] Textures;
+    public GameObject CardTemplate;
+
+	// Use this for initialization
+	void Start () {
+        float offset = 0f;
+		foreach (Texture texture in Textures)
+        {
+            var card = Instantiate(CardTemplate);
+            card.GetComponent<MeshRenderer>().material.mainTexture = texture;
+            card.transform.localPosition = new Vector3((offset % 7f) / 10f, 0f, (offset - (offset % 7f)) / 70f);
+            offset += 1f;
+        }
+	}
+}
